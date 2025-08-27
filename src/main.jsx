f4c7dbf102042c373d01a5a2b9928648980a2d59
Vithushan Kanesamoorthy <vithuvithush44@gmail.com>
2025-08-27 22:12:26 +0530

    final changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router-dom"; // <-- Change here
 
 createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</StrictMode>
+  <StrictMode>
+    <HashRouter> {/* <-- Use HashRouter instead of BrowserRouter */}
+      <App />
+    </HashRouter>
+  </StrictMode>
 );
